1e5f39f179b280fb72be65e7d1bdbbd51d21e753
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2022-06-21 22:50:24 +0530

    await pattern v 3.0

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,16 +1,12 @@
-const { readFile, writeFile } = require("fs");
-const util = require("util");
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
-
+const { readFile, writeFile } = require("fs").promises;
 //Custom Async Promise & Refactor to Async
 // CHECK FILE PATH!!!! [file path is mtach while using this code in app.js]
 
 const start = async () => {
   try {
-    const first = await readFilePromise("./content/first.txt", "utf8");
-    const second = await readFilePromise("./content/second.txt", "utf8");
-    await writeFilePromise(
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
       "./content/async-result-mind-grenade.txt",
       `Woo hoo : ${first} ${second}`
     );
