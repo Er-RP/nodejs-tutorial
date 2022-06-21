d115b2db4cf6924e218d81081483c34daa6c0d11
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2022-06-21 10:52:42 +0530

    http.js with dynamic response

--- 12-http.js
+++ 12-http.js
@@ -1,8 +1,20 @@
 const http = require("http"); //http modules are used to create the server instances
 
 const server = http.createServer((req, res) => {
-  res.write("Welcome to homepage");
-  res.end();
+  // console.log(req)
+  /* Sending diffrent response based on the request url */
+  if (req.url === "/") {
+    res.end("Welcome to homepage");
+  } else if (req.url === "/about") {
+    res.end("This is aboutPage");
+  } else {
+    console.log("Not Found");
+    res.end(`
+      <h1>Oops!</h1>
+      <p>We can't seem to find the page you are looking for</p>
+      <a href="/">back home</a>
+      `);
+  }
 });
 
 server.listen(5000); // Run http server locally at port 5000
