5ea2bdbb7fee97afd3e20d2e79c3f1d3aed1ca53
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2023-02-23 17:17:20 +0530

    nothing

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbURI: "mongodb+srv://rajapandi:96590929Rp@cluster0.da1fk.mongodb.net/jwt",
+  dbURI: "",
   saltRounds: 10,
   jwtSecret: "SECRET",
   jwtTokenMaxAge: 3 * 24 * 60 * 60,
